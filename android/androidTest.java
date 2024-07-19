c0f0b164daee407621af8cfe5264eaafd1207db2
Manoj <manimanoj1851@gmail.com>
2024-07-19 15:28:48 +0530

    IQOO NEO 9 IS THE BEST ANDROID DEVICE

--- android/androidTest.java
+++ android/androidTest.java
@@ -4,8 +4,9 @@ public class androidTest {
     public static void main(String[] args) {
         Android A1= new Android();
         A1.brand="iqoo";
-        A1.name="neo 9 ultra pro max";
+        A1.name="neo 9 ultra max";
         A1.price=50000;
+
         System.out.println(A1);
     }
 }
