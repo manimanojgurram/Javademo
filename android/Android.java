4f4b79f0047c109b3c732fd507a5793217d3f5a8
Manoj <manimanoj1851@gmail.com>
2024-07-19 15:27:18 +0530

    IQOO NEO 9 IS THE BEST ANDROID DEVICE

---
+++ android/Android.java
@@ -0,0 +1,16 @@
+package android;
+
+public class Android {
+    public String brand;
+    public String name;
+    public int price;
+
+    @Override
+    public String toString() {
+        return "Android{" +
+                "brand='" + brand + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ android/androidTest.java
@@ -0,0 +1,11 @@
+package android;
+
+public class androidTest {
+    public static void main(String[] args) {
+        Android A1= new Android();
+        A1.brand="iqoo";
+        A1.name="neo 9 ultra pro max";
+        A1.price=50000;
+        System.out.println(A1);
+    }
+}
